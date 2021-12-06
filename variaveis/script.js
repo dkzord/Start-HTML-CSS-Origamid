c1d0c0098948cee7f75f0a7cc89fd88ca91f94ec
Fernando Cavalcanti(DK) <comododk@gmail.com>
2021-12-06 18:10:47 -0300

    variabel

---
+++ variaveis/script.js
@@ -0,0 +1,62 @@
+/* // Declare uma variável contendo uma string
+var nome = "Brasil!";
+
+// Declare uma variável contendo um número dentro de uma string
+var numString = "55";
+
+// Declare uma variável com a sua idade
+var idade = 27;
+
+console.log(typeof numString);
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var myName = "Fernando";
+var myLastName = "Cavalcanti";
+
+console.log(`Meu nome é ${myName} ${myLastName}`);
+// Coloque a seguinte frase em uma variável: It's time
+var phrase = "It's time";
+
+console.log(phrase);
+// Verifique o tipo da variável que contém o seu nome
+var testString = typeof myName;
+
+console.log(testString);
+ */
+
+
+/* 
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+
+console.log(total);
+// Crie duas expressões que retornem NaN
+var fraseNaN = "20s";
+var phrase = fraseNaN / 2;
+
+console.log(phrase);
+console.log(isNaN(phrase));
+// Somar a string '200' com o número 50 e retornar 250
+var stringFrase = +'200';
+// +stringFrase; 
+
+var total2 = stringFrase + 50;
+
+console.log(total2);
+// Incremente o número 5 e retorne o seu valor incrementado
+var increment = 5;
+
+console.log(++increment);
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
++numero;
+
+var pesoPorDois = numero / 2; // NaN (Not a Number)
+
+console.log(pesoPorDois);
+ */
+
