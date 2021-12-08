3dcd4fae8d0f27e1010807afca62275deb3cedb3
Fernando Cavalcanti(DK) <comododk@gmail.com>
2021-12-08 19:56:26 -0300

    Last att

--- variaveis/script-object.js
+++ variaveis/script-object.js
@@ -31,8 +31,55 @@ for(let count = 0; count < frutas.length; count++){
   }
 }
 
-// Coloque a última fruta da array acima em uma variável,
+/* // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
 
-var lastFruit = frutas[frutas.length - 1]; */
+var lastFruit = frutas[frutas.length - 1];
 
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var scroll = 1000;
+scroll += 500;
+
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito = (possuiCarro && possuiCasa);
+
+console.log(darCredito); */
+
+
+
+
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas);
+}
+
+// Como corrigir o erro abaixo?
+const dois = 2;
+
+function somarDois(x) {
+  return x + dois;
+}
+
+function dividirDois(x) {
+  return x / dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(6));
+
+// O que fazer para total retornar 500?
+const numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
