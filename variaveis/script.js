7b3c74c973e56f30ade8ae22e97e580c4c28f27b
Fernando Cavalcanti(DK) <comododk@gmail.com>
2021-12-07 21:15:35 -0300

    Funcations

--- variaveis/script.js
+++ variaveis/script.js
@@ -139,3 +139,78 @@ function corFavorita(cor){
 
 addEventListener('click', function(){ console.log('oi!')}) */
 
+/* function terceiraIdade(idade){
+  if (typeof idade !== 'number'){
+    return 'Informa um número valido!';
+  } else if ( idade >= 60 ){
+    return 'Tem 60 ou mais anos!';
+  } else {
+    return `Tem apenas ${idade} anos`;
+  }
+}
+
+console.log(terceiraIdade(75)); */
+
+// Exercícios Funções
+
+// Crie uma função para verificar se um valor é Truthy
+function valorPositivo(valor){
+  if (valor >= 0){
+    return true;
+  } else { 
+    return false;
+  }
+}
+
+console.log(valorPositivo(5));
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetroQuadrado(lado){
+  return lado * 4;
+}
+
+console.log(perimetroQuadrado(5))
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(firstName, lastName) {
+  return `Seu nome é ${firstName} ${lastName}`;
+}
+
+console.log(nomeCompleto('Fernando', 'Cavalcanti'));
+// Crie uma função que verifica se um número é par
+function numeroPar(number){
+  if(typeof number !== 'number'){
+    return 'Coloque um número valido!';
+  } else if(( number % 2 ) == 0){
+    return `O número ${number} é par!`;
+  } else {
+    return `O número ${number} é impar!`;
+  }
+}
+
+console.log(numeroPar(3));
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+function tipoDado(dado){
+  return typeof dado;
+}
+
+console.log(tipoDado('pao'));
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+addEventListener('scroll', function() { console.log('Fernando Cavalcanti')})
+
+// Corrija o erro abaixo
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+
+function jaVisitei(paisesVisitados) {
+  var totalPaises = 193;
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+console.log(precisoVisitar(20));
+console.log(jaVisitei(20));
