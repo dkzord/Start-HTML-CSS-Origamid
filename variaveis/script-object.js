09f21c4c4245f4f10c6c5e65e933a3d908728700
Fernando Cavalcanti(DK) <comododk@gmail.com>
2021-12-08 18:58:50 -0300

    For

---
+++ variaveis/script-object.js
@@ -0,0 +1,38 @@
+// Array
+/* 
+var variedade = ['Switch', 'Xbox', 'PS4', '3DS'];
+var frutas = ['Banana', 'Pera','Maçã', 'Abacaxi','Uva'];
+
+// for (let i = 0; i < variedade.length; i++) {
+//   console.log(variedade[i]);
+// }
+
+frutas.forEach( function(item, index) {
+  console.log(item, index);
+}); 
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var copaBrasil = ['1959', '1960', '1970', '1994', '2002'];
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+copaBrasil.forEach(function(yearWinCopa) {
+  console.log(`O Brasil ganhou a copa de ${yearWinCopa}`);
+});
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'];
+
+for(let count = 0; count < frutas.length; count++){
+  console.log(`${frutas[count]}`)
+  if(frutas[count] === 'Pera'){
+    break;
+  }
+}
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+var lastFruit = frutas[frutas.length - 1]; */
+
